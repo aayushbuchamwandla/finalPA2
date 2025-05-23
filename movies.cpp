7482f968834ec16e2f6889a47f8c1433a855bfba
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2025-05-23 16:11:38 -0700

    Update movies.cpp

--- movies.cpp
+++ movies.cpp
@@ -1,20 +1,19 @@
-#include "movies.h"
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include "movies.h"
+using namespace std;
 
-bool compareMoviesForOutput(const Movie& m1, const Movie& m2) {
-    if (m1.rating != m2.rating) {
-        return m1.rating > m2.rating;
-    } else {
-        return m1.name < m2.name;
+bool operations(Movie m1, Movie m2) {
+    if (m1.rating!=m2.rating) {
+        return m1.rating>m2.rating;
+    }
+    else {
+        return m1.name<m2.name;
     }
 }
 
-void PrintByRating(std::vector<Movie>& m) {
-    std::sort(m.begin(), m.end(), compareMoviesForOutput);
-
-    for (const auto& movie : m) {
-        std::cout << movie.name << ", " << movie.rating << std::endl;
+void PrintByRating(vector<Movie> &m) {
+    sort(m.begin(),m.end(),operations);
+    for (int i=0; i<m.size(); i++) {
+        cout<<m.at(i).name<<", "<<m.at(i).rating<<endl;
     }
 }
