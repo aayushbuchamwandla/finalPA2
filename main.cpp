c3f3b462730db7f440efeee86806ece2fc6775fe
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2025-05-23 16:16:47 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <ctime>
 #include <vector>
-#include <cstring>
 #include <algorithm>
-#include <limits.h>
 #include <iomanip>
-#include <set>
-#include <queue>
-#include <sstream>
+#include <sstream>   
+
+#include "movies.h"  // Include our Movie struct and PrintByRating function
+
 using namespace std;
 
-#include "movies.h"
+// Function to parse a line from the CSV file
+bool parseLine(string &line, string &movieName, double &movieRating) {
+    int commaIndex = line.find_last_of(",");
+    if (commaIndex == string::npos) {
+        return false;
+    }
+    movieName = line.substr(0, commaIndex);
+    try {
+        movieRating = stod(line.substr(commaIndex + 1));
+    } catch (const invalid_argument& e) {
+        cerr << "Invalid rating format: " << line.substr(commaIndex + 1) << endl;
+        return false;
+    } catch (const out_of_range& e) {
+        cerr << "Rating out of range: " << line.substr(commaIndex + 1) << endl;
+        return false;
+    }
 
-bool parseLine(string &line, string &movieName, double &movieRating);
+    if (movieName.length() >= 2 && movieName[0] == '\"' && movieName.back() == '\"') {
+        movieName = movieName.substr(1, movieName.length() - 2);
+    }
+    return true;
+}
 
 int main(int argc, char** argv){
     if (argc < 2){
@@ -27,37 +44,27 @@ int main(int argc, char** argv){
     }
 
     ifstream movieFile (argv[1]);
- 
+
     if (movieFile.fail()){
         cerr << "Could not open file " << argv[1];
         exit(1);
     }
-    // Create an object of a STL data-structure to store all the movies
 
     string line, movieName;
     double movieRating;
-    // Read each file and store the name and rating
 
-    map<string,double> movies;
     vector<Movie> movies2;
-    while (getline (movieFile, line) && parseLine(line, movieName, movieRating)){
-            // Algorithm 1 (movies[movieName] = movieRating;)
-            movies[movieName] = movieRating;
-            //Algorithm 2
-            movies2.push_back(Movie(movieName, movieRating));
-            // Use std::string movieName and double movieRating
-            // to construct your Movie objects
-            // cout << movieName << " has rating " << movieRating << endl;
-            // insert elements into your data structure
-    }
 
+    while (getline (movieFile, line) && parseLine(line, movieName, movieRating)){
+        movies2.push_back(Movie(movieName, movieRating));
+    }
     movieFile.close();
 
+    std::sort(movies2.begin(), movies2.end());
+
     if (argc == 2){
-        //Algorithm 1
-        //print all the movies in ascending alphabetical order of movie names
-        for (auto it = movies.begin(); it != movies.end(); ++it) {
-            cout<<it->first<<", "<<it->second<<endl;
+        for (const auto& movie : movies2) {
+            cout << movie.name << ", " << movie.rating << endl;
         }
         return 0;
     }
@@ -75,44 +82,60 @@ int main(int argc, char** argv){
             prefixes.push_back(line);
         }
     }
+    prefixFile.close();
 
-    vector<Movie> shortened;
     vector<string> finalLines;
+
     for (int i=0; i<prefixes.size(); i++) {
-        for (int j=0; j<movies2.size(); j++) {
-            if ((movies2.at(j).name).find(prefixes.at(i))==0) {
-                shortened.push_back(Movie(movies2.at(j).name,movies2.at(j).rating));
+        const string& prefix = prefixes.at(i);
+
+        auto lower_it = std::lower_bound(movies2.begin(), movies2.end(), Movie(prefix, 0.0),
+            [](const Movie& m, const Movie& p) {
+                return m.name < p.name;
+            });
+
+        string upper_bound_str = prefix;
+        if (!upper_bound_str.empty()) {
+            size_t last_char_idx = upper_bound_str.length() - 1;
+            char last_char = upper_bound_str[last_char_idx];
+            if (last_char == '~' || last_char == 255) {
+                upper_bound_str += '~';
+            } else {
+                upper_bound_str[last_char_idx]++;
+            }
+        } else {
+            upper_bound_str = "~";
+        }
+
+        auto upper_it = std::upper_bound(movies2.begin(), movies2.end(), Movie(upper_bound_str, 0.0),
+            [](const Movie& m, const Movie& p) {
+                return m.name < p.name;
+            });
+
+        vector<Movie> shortened;
+        for (auto it = lower_it; it != upper_it; ++it) {
+            if (it->name.find(prefix) == 0) {
+                shortened.push_back(*it);
             }
         }
+
         PrintByRating(shortened);
+
         if (shortened.size()!=0) {
             stringstream ss;
             ss.str("");
             ss.clear();
-            ss<<"Best movie with prefix "<<prefixes.at(i)<<" is: "<<shortened.at(0).name<<" with rating "<<std::fixed<<std::setprecision(1)<<shortened.at(0).rating;
-            line = ss.str();
-            finalLines.push_back(line);
+            ss<<"Best movie with prefix "<<prefix<<" is: "<<shortened.at(0).name<<" with rating "<<std::fixed<<std::setprecision(1)<<shortened.at(0).rating;
+            finalLines.push_back(ss.str());
+            cout<<endl;
+        } else {
+            cout<<"No movies found with prefix " + prefix;
         }
-        else {
-            cout<<"No movies found with prefix " + prefixes.at(i);
-        }
-        shortened.clear();
-        cout<<endl;
     }
-    for (string s: finalLines) {
-        cout<<s<<endl;
+
+    for (string s : finalLines) {
+        cout << s << endl;
     }
+
     return 0;
-}
-
-/* Add your run time analysis for part 3 of the assignment here as commented block*/
-
-bool parseLine(string &line, string &movieName, double &movieRating) {
-    int commaIndex = line.find_last_of(",");
-    movieName = line.substr(0, commaIndex);
-    movieRating = stod(line.substr(commaIndex+1));
-    if (movieName[0] == '\"') {
-        movieName = movieName.substr(1, movieName.length() - 2);
-    }
-    return true;
 }
