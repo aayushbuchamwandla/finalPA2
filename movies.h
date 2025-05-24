eb0ff010895ab442d48c290e736b59cb40eedb0d
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2025-05-23 17:06:34 -0700

    Update movies.h

--- movies.h
+++ movies.h
@@ -5,23 +5,20 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 struct Movie {
-    std::string name;
+    string name;
     double rating;
 
-    Movie(std::string name, double rating) : name(name), rating(rating) {}
+    Movie(string name, double rating) : name(name), rating(rating) {}
     
-    // For sorting by name
     bool operator<(const Movie& other) const {
         return name < other.name;
     }
 };
 
-// For sorting by rating then name
 bool compareByRating(const Movie& a, const Movie& b);
+void findMoviesByPrefix(const vector<Movie>& movies, const string& prefix, vector<Movie>& results);
 
-// Binary search based prefix search
-void findMoviesByPrefix(const std::vector<Movie>& movies, const std::string& prefix, 
-                       std::vector<Movie>& results);
-
-#endif // MOVIES_H
+#endif
